6ce9e81a15628f6cc6a1c43557d63210d28c4be2
Adam Palmer <theadampalmer@gmail.com>
2020-07-26 20:23:58 +0000

    Automatically backed up by Learn

--- lib/Concerns/findable.rb
+++ lib/Concerns/findable.rb
@@ -1,11 +1,9 @@
-module Concerns
-  module Findable
-    def find_by_name(name)
-      all.detect { |song| song.name == name }
-    end
+module Concerns::Findable
+  def find_by_name(name)
+    all.detect{ |song| song.name == name }
+  end
 
-    def find_or_create_by_name(name)
-      find_by_name(name) || create(name)
-    end
+  def find_or_create_by_name(name)
+    find_by_name(name) || create(name)
   end
 end 
