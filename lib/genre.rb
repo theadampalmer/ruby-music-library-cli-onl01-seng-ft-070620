cdf2678cc1165005c10f40093ea1f328405d297d
Adam Palmer <theadampalmer@gmail.com>
2020-07-26 20:06:44 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,33 @@
+class Genre 
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+  
+  def genres
+    songs.map(&:artist).uniq
+  end
+  
+end
