5aaecfdfd713817856db0620943fd508b61cb1d6
Adam Palmer <theadampalmer@gmail.com>
2020-07-26 21:49:22 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre 
+  extend Concerns::Findable
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+  
+  def artists
+    songs.map(&:artist).uniq
+  end
+  
+end
